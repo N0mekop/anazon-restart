ff8c881a96f4ea67765af8371a9186ff435e7aa7
N0mekop <blinkevin@hotmail.com>
2023-01-10 13:56:42 +0100

    Symfony composer req orm

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
     #[Route('/hello/{name<\w+>}', methods: ['GET'], name: 'app_hello')]
-    public function index(string $name): Response
+    public function index(Request $request, string $name = 'Kevin'): Response
     {
         return $this->render('hello/index.html.twig', [
             'controller_name' => 'HelloController',
